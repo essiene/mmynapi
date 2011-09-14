061ed88da861f9a473620fd2c0175c79ef077769
Essien Ita Essien <essiene@gmail.com>
2011-09-14 13:54:30 +0100

    Implement mmynapi:call/2
    Using ibrowse, POST the #'mmyn.message'{} JSON serialization
    to endpoint URL.
    
    - Support HTTP BASIC AUTH via ibrowse
    - Send proper content type and accept headers
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/mmynapi.erl
+++ src/mmynapi.erl
@@ -1,7 +1,37 @@
 -module(mmynapi).
--export([to_json/1, to_json/3, from_json/1]).
+-export([to_json/1, to_json/3, from_json/1,call/2]).
 
 -include("mmynapi.hrl").
+
+call({Url, User, Pass}, #'mmyn.message'{}=Msg) ->
+    C = #callopts{url=Url, opts=[{basic_auth, {User, Pass}}]},
+    http_call(C, Msg);
+call(Url, #'mmyn.message'{}=Msg) ->
+    C = #callopts{url=Url},
+    http_call(C, Msg).
+
+http_call(#callopts{}=C, #'mmyn.message'{}=Msg) ->
+    Req = to_json(Msg),
+    case http_call(C, Req) of
+        {error, Reason} ->
+            {error, Reason};
+        {ok, Json} ->
+            from_json(Json)
+    end;
+http_call(#callopts{url=Url, headers=Headers0, opts=Opts0}, Body) ->
+    Opts1 = [{response_format, binary}|Opts0],
+    Headers1 = [{"Content-Type", "application/json"}, {"Accept", "application/json"}|Headers0],
+    try ibrowse:send_req(Url, Headers1, post, Body, Opts1) of
+        {error, Reason} ->
+            {error, Reason};
+        {ok, "200", _, ResponseBody} ->
+            {ok, ResponseBody};
+        {ok, StatusCode, _, _StatusDetail} ->
+            {error, list_to_atom(lists:concat([http_,StatusCode]))}
+        catch 
+            _:Exception ->
+                {error, Exception}
+        end.
 
 
 to_json(System, Tid, #'req.sendsms'{}=B) ->
